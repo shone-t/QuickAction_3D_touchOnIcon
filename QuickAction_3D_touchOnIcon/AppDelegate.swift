bed547475731e075ce240a82c136e0c9f7fefa78
Shone <shonesrbija@yahoo.com>
2019-08-15 15:38:35 +0200

    finish wit navigation

--- QuickAction_3D_touchOnIcon/AppDelegate.swift
+++ QuickAction_3D_touchOnIcon/AppDelegate.swift
@@ -12,33 +12,53 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var vcsArray = [UIViewController]()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let prvi = storyboard.instantiateViewController(withIdentifier: "prviVC") as! prviVC
+        let drugi = storyboard.instantiateViewController(withIdentifier: "drugiVC") as! drugiVC
+        let treci = storyboard.instantiateViewController(withIdentifier: "treciVC") as! treciVC
+        
+        vcsArray = [prvi, drugi, treci]
+        
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    enum ShortcutType : String {
+        case mountains = "mountains"
+        case space = "space"
+        case ocean = "ocean"
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        
+        let navVC = window?.rootViewController as! UINavigationController
+        navVC.setViewControllers(vcsArray, animated: false)
+        
+        //com.jonnyb.Shortcut.mountains
+        if let type = shortcutItem.type.components(separatedBy: ".").last {
+            switch type {
+            
+            case ShortcutType.space.rawValue:
+                //print("this is space.")
+                navVC.popToViewController(vcsArray[1], animated: true)
+                completionHandler(true)
+            case ShortcutType.ocean.rawValue:
+                //print("Love the beach.")
+                navVC.popToViewController(vcsArray[2], animated: true)
+                completionHandler(true)
+            default:
+            //print("these are the mountains")
+                navVC.popToRootViewController(animated: true)
+                completionHandler(true)
+            }
+        }
+        completionHandler(false)
+        
     }
 
 
