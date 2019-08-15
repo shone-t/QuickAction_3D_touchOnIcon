55fbc039fb57d2c0ec083ee8939d68c2663408a9
Shone <shonesrbija@yahoo.com>
2019-08-15 15:57:18 +0200

    shortcut for tabs

--- QuickAction_3D_touchOnIcon/AppDelegate.swift
+++ QuickAction_3D_touchOnIcon/AppDelegate.swift
@@ -16,31 +16,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let oceanShortcut = UIMutableApplicationShortcutItem(type: "\(String(describing: Bundle.main.bundleIdentifier)).ocean", localizedTitle: "Ocean", localizedSubtitle: "subtitle 3", icon: UIApplicationShortcutIcon.init(templateImageName: "OceanShort"), userInfo: nil)
+        let spaceShortcut = UIMutableApplicationShortcutItem(type: "\(String(describing: Bundle.main.bundleIdentifier)).space", localizedTitle: "Space", localizedSubtitle: "subtitle 2", icon: UIApplicationShortcutIcon.init(templateImageName: "SpaceShort"), userInfo: nil)
+        let mountainShortcut = UIMutableApplicationShortcutItem(type: "\(String(describing: Bundle.main.bundleIdentifier)).mountain", localizedTitle: "Mountain", localizedSubtitle: "subtitle 1", icon: UIApplicationShortcutIcon.init(templateImageName: "MountainShort"), userInfo: nil)
+        application.shortcutItems = [mountainShortcut, spaceShortcut, oceanShortcut]
         return true
+        
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    enum ShortcutType : String {
+        case mountains = "mountains"
+        case space = "space"
+        case ocean = "ocean"
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+ 
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if let type = shortcutItem.type.components(separatedBy: ".").last {
+            
+            let navVc = window?.rootViewController as! UITabBarController
+            
+            switch type {
+            case ShortcutType.space.rawValue:
+                navVc.selectedIndex = 1
+                completionHandler(true)
+            case ShortcutType.ocean.rawValue:
+                navVc.selectedIndex = 2
+                completionHandler(true)
+            default:
+                navVc.selectedIndex = 0
+                completionHandler(true)
+            }
+            completionHandler(false)
+            
+        }
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
